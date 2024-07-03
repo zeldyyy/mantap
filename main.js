d0e87c56eb14924b71ceb7c7a1ea58f1351a7008
QuantumFlareTedi <147927478+zeldyyy@users.noreply.github.com>
2024-07-03 18:27:22 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById('button').addEventListener('click',function(){
+    document.querySelector('.hb').classList.toggle('bh')
+    document.querySelector('.btn').classList.toggle('bt')            
+})
